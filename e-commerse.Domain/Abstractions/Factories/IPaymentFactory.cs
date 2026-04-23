291537071a62dc75ee8d82a2de9053e2826e30ab
Abderhman <bebosherif202@gmail.com>
2026-04-23 20:13:13 +0200

    add payment factory interface

---
+++ e-commerse.Domain/Abstractions/Factories/IPaymentFactory.cs
@@ -0,0 +1,15 @@
+﻿using e_commerse.Domain.Entities;
+using e_commerse.Domain.Enums.Payment;
+using e_commerse.Domain.ValueObjects.Order;
+using e_commerse.Domain.ValueObjects.Payment;
+using e_commerse.Domain.ValueObjects.Product;
+using e_commerse.Domain.ValueObjects.User;
+
+namespace e_commerse.Domain.Abstractions.Factories
+{
+    public interface IPaymentFactory
+    {
+        Payment Create(PaymentId id, OrderId orderId, UserId userId,
+            Money amount, PaymentProvider provider);
+    }
+}
