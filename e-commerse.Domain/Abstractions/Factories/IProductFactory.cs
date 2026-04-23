303ae598bf32df6048367168f95ad150f72c71eb
Abderhman <bebosherif202@gmail.com>
2026-04-23 19:40:02 +0200

    add user factory interface

--- e-commerse.Domain/Abstractions/Factories/IProductFactory.cs
+++ e-commerse.Domain/Abstractions/Factories/IProductFactory.cs
@@ -1,10 +1,14 @@
 ﻿using e_commerse.Domain.Entities;
+using e_commerse.Domain.ValueObjects.Category;
+using e_commerse.Domain.ValueObjects.Product;
 
 namespace e_commerse.Domain.Abstractions.Factories
 {
     public interface IProductFactory
     {
-        public Product CreateDefault();
-        public Product CreateWithCategory();
+        public Product CreateDefault(ProductId id, ProductName name, ProductDescription description
+            StockQuantity stockQuantity, Money money);
+        public Product CreateWithCategory(ProductId id, ProductName name, ProductDescription description
+            StockQuantity stockQuantity, Money money, CategoryId categoryId);
     }
 }
---
+++ e-commerse.Domain/Abstractions/Factories/IUserFactory.cs
@@ -0,0 +1,11 @@
+﻿using e_commerse.Domain.Entities;
+using e_commerse.Domain.Enums.User;
+using e_commerse.Domain.ValueObjects.User;
+
+namespace e_commerse.Domain.Abstractions.Factories
+{
+    public interface IUserFactory
+    {
+        User Create(UserId id, UserFullName fullName, Email email, UserRole role);
+    }
+}
