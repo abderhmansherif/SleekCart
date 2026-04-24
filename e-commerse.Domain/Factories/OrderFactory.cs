ad6e81f52dea27646928beb8f5151ff1b062a362
Abderhman <bebosherif202@gmail.com>
2026-04-24 19:17:50 +0300

    add factory implementation for Order entity

---
+++ e-commerse.Domain/Factories/OrderFactory.cs
@@ -0,0 +1,16 @@
+﻿using e_commerse.Domain.Abstractions.Factories;
+using e_commerse.Domain.Entities;
+using e_commerse.Domain.ValueObjects.Order;
+using e_commerse.Domain.ValueObjects.User;
+
+namespace e_commerse.Domain.Factories
+{
+    public class OrderFactory : IOrderFactory
+    {
+        public Order CreateDefault(OrderId id, UserId userId, ShippingAddress shippingAddress)
+            => new Order(id, userId, shippingAddress);
+
+        public Order CreateWithItems(OrderId id, UserId userId, ShippingAddress shippingAddress, List<OrderItem> items)
+            => new Order(id, userId, shippingAddress, items);
+    }
+}
