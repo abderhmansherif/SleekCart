2deb36dae9715bf43f744f8f3d2dd1cecbd414e7
Abderhman <bebosherif202@gmail.com>
2026-04-24 22:03:42 +0300

    add StockReservation value object for Product entity

---
+++ e-commerse.Domain/ValueObjects/Product/StockReservation.cs
@@ -0,0 +1,18 @@
+﻿using e_commerse.Domain.ValueObjects.Cart;
+
+namespace e_commerse.Domain.ValueObjects.Product
+{
+    public record StockReservation
+    {
+        public CartId CartId { get; private set; }
+        public int Quantity { get; private set; }
+        public DateTime ExpiryDate { get; private set; }
+
+        public StockReservation(CartId cartId, int quantity, DateTime expiryDate)
+        {
+            CartId = cartId;
+            Quantity = quantity;
+            ExpiryDate = expiryDate;
+        }
+    }
+}
