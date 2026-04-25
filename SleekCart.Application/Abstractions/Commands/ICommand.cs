ff2c9586cf770abb7f4f72dc6cc992fba6d0d477
Abderhman <bebosherif202@gmail.com>
2026-04-25 20:00:28 +0300

    add Application layer

--- SleekCart.Application/Abstractions/Commands/ICommand.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SleekCart.Application.Abstractions.Commands
-{
-    public interface ICommand
-    {
-    }
-}
--- SleekCart.Application/Abstractions/Commands/ICommandHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SleekCart.Application.Abstractions.Commands
-{
-    public interface ICommandHandler<TCommand> where TCommand : ICommand
-    {
-        Task HandleAsync(TCommand command);
-    }
-}
