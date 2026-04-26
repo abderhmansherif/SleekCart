e3f0d211064b901c9a7d36825517c6df8cbf3aaf
Abderhman <bebosherif202@gmail.com>
2026-04-26 23:07:21 +0300

    setup project structure

---
+++ SleekCart.Application/Extension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SleekCart.Application.Abstractions.Commands;
+using SleekCart.Shared.Commands;
+
+namespace SleekCart.Application
+{
+    public static class Extension
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddCommands();
+
+            var assembly = typeof(Extension).Assembly;
+            services.Scan( s=> s.FromAssembliesOf(typeof(Extension))
+                .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+
+            return services;
+        }
+    }
+}
--- SleekCart.Shared/Commands/Extension.cs
+++ SleekCart.Shared/Commands/Extension.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SleekCart.Application.Abstractions.Commands;
-using System.Reflection;
 
 namespace SleekCart.Shared.Commands
 {
@@ -8,16 +7,7 @@ namespace SleekCart.Shared.Commands
     {
         public static IServiceCollection AddCommands(this IServiceCollection services)
         {
-            services.AddSingleton<ICommandDispatcher, InMemoryCommandDispatcher>();
-
-            var assembly = typeof(ICommandHandler<>).Assembly;
-
-            // Register all ICommandHandler<> implementations in the assembly
-            services.Scan(s => s.FromAssemblies(assembly)
-                    .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
-                    .AsImplementedInterfaces()
-                    .WithScopedLifetime());
-
+            services.AddScoped<ICommandDispatcher, InMemoryCommandDispatcher>();
             return services;
         }
     }
