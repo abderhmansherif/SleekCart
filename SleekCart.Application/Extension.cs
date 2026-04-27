266bf92fe328461b02bfce977d1ca8dad6476820
Abderhman <bebosherif202@gmail.com>
2026-04-27 19:00:03 +0300

    Registers User Commands Validators

--- SleekCart.Application/Extension.cs
+++ SleekCart.Application/Extension.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using SleekCart.Application.Abstractions.Commands;
+using SleekCart.Application.Commands.LoginUser;
+using SleekCart.Application.Commands.RegisterUser;
+using SleekCart.Application.RegisterUser;
 using SleekCart.Shared.Commands;
 
 namespace SleekCart.Application
@@ -9,9 +13,13 @@ namespace SleekCart.Application
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddCommands();
+            
+            services.AddScoped<IValidator<RegisterUserCommand>, RegisterUserValidator>();
+            services.AddScoped<IValidator<LoginUserCommand>, LoginUserValidator>();
+
+
 
             var assembly = typeof(Extension).Assembly;
-            
             services.Scan( s=> s.FromAssembliesOf(typeof(Extension))
                 .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
                 .AsImplementedInterfaces()
