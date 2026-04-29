ad81c223f67e51560c7c3a1b0885995ec4ec00a4
Abderhman <bebosherif202@gmail.com>
2026-04-29 20:53:04 +0300

    Register The Validators

--- SleekCart.Application/Extension.cs
+++ SleekCart.Application/Extension.cs
@@ -1,10 +1,13 @@
 ﻿using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using SleekCart.Application.Abstractions.Commands;
-using SleekCart.Application.Commands.LoginUser;
-using SleekCart.Application.Commands.RefreshToken;
-using SleekCart.Application.Commands.RegisterUser;
-using SleekCart.Application.RegisterUser;
+using SleekCart.Application.Commands.Authentication.LoginUser;
+using SleekCart.Application.Commands.Authentication.RefreshToken;
+using SleekCart.Application.Commands.Authentication.RegisterUser;
+using SleekCart.Application.Commands.User.AddAddress;
+using SleekCart.Application.Commands.User.UpdateProfile;
+using SleekCart.Application.Queries.User.GetMyAddresses;
+using SleekCart.ApplicationCommands.Authentication.RegisterUser;
 using SleekCart.Shared.Commands;
 
 namespace SleekCart.Application
@@ -18,6 +21,10 @@ namespace SleekCart.Application
             services.AddScoped<IValidator<RegisterUserCommand>, RegisterUserValidator>();
             services.AddScoped<IValidator<LoginUserCommand>, LoginUserValidator>();
             services.AddScoped<IValidator<RefreshTokenCommand>, RefreshTokenValidator>();
+            services.AddScoped<IValidator<GetMyAddressesQuery>, GetMyAddressesValidator>();
+            services.AddScoped<IValidator<AddAddressCommand>, AddAddressValidator>();
+            services.AddScoped<IValidator<UpdateProfileCommand>, UpdateProfileValidator>();
+
 
             var assembly = typeof(Extension).Assembly;
             services.Scan( s=> s.FromAssembliesOf(typeof(Extension))
--- SleekCart.Application/Mappers/UserAddressesMapper.cs
+++ SleekCart.Application/Mappers/UserAddressesMapper.cs
@@ -6,15 +6,14 @@ namespace SleekCart.Application.Mappers;
 public static class UserAddressesMapper
 {
     public static List<AddressDto> ToAddressDtos(this User user)
-    {
-        return user.Addresses.Select(a => new AddressDto
-        {
-            AddressId = a.Id,
-            City = a.City.Value,
-            Street = a.Street.Value,
-            Building = a.Building.Value,
-            Note = a.Note.Value
+        => user.Addresses.Select(a => new AddressDto
+            {
+                AddressId = a.Id,
+                City = a.City.Value,
+                Street = a.Street.Value,
+                Building = a.Building.Value,
+                Note = a.Note.Value
 
-        }).ToList();
-    }
+            }).ToList();
+        
 }
