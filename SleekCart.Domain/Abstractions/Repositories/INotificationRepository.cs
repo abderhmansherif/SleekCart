6686e7d2f72d6509cbf2456535cfe5c32056c632
Abderhman <bebosherif202@gmail.com>
2026-07-20 18:45:49 +0300

    Update INotification Repo

--- SleekCart.Domain/Abstractions/Repositories/INotificationRepository.cs
+++ SleekCart.Domain/Abstractions/Repositories/INotificationRepository.cs
@@ -7,6 +7,7 @@ namespace e_commerse.Domain.Abstractions.Repositories
     public interface INotificationRepository
     {
         Task<Notification> GetAsync(NotificationId notificationId, UserId userId,  CancellationToken ct);
+        Task<List<Notification>> GetAllByUserIdAsync(UserId userId, CancellationToken ct);
         Task InsertAsync(Notification notification, CancellationToken ct);
         Task UpdateAsync(Notification notification, CancellationToken ct);
         Task DeleteAsync(NotificationId notificationId, CancellationToken ct);
